e985e5a00a2b0bb84bfa146d45d6589606f90d87
kafcok <kafcok@gmail.com>
2026-02-12 01:47:54 +0100

    test2

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,23 @@
-// import { useState } from "react";
+import { useState } from "react";
 
 function App() {
-  // const [value, setValue] = useState(0);
+  const [value, setValue] = useState(0);
 
-  // function handleClick() {
-  //   setValue((v) => {
-  //     return v + 1;
-  //   });
-  //   setValue((v) => {
-  //     return v + 1;
-  //   });
-  // }
+  function handleClick() {
+    setValue((v) => {
+      return v + 1;
+    });
+    setValue((v) => {
+      return v + 1;
+    });
+  }
 
-  return <h1>test</h1>;
+  return (
+    <>
+      <h1>test</h1>
+      <button onClick={handleClick}>{value}</button>
+    </>
+  );
 }
 
 export default App;
