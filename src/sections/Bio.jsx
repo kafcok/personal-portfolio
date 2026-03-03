b814e11856b7fc81a3a2d89fdb14ff4e352f82fa
kafcok <kafcok@gmail.com>
2026-03-03 12:01:27 +0100

    bio pdf hypen swap

--- src/sections/Bio.jsx
+++ src/sections/Bio.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { MainContext } from "../Contexts";
 import { useBio } from "../hooks/useGetData";
 import Spinner from "../Spinner";
@@ -7,8 +7,20 @@ import * as Icon from "../Icons";
 
 export default function Bio() {
   const q_key = "bio";
-  const { lang } = useContext(MainContext);
+  const { lang, isPdf } = useContext(MainContext);
   const { isLoading, data, error } = useBio({ q_key, lang });
+  const [bioContent, setBioContent] = useState({ __html: "" });
+
+  useEffect(
+    function () {
+      let temp = data?.[`description_${lang}`];
+      if (isPdf) {
+        temp.replace("&#8209;", "-");
+      }
+      setBioContent({ __html: temp });
+    },
+    [data, isPdf, lang],
+  );
 
   if (error) {
     return <p className="text-error">{error.message}</p>;
@@ -24,9 +36,7 @@ export default function Bio() {
         text={data?.[`header_${lang}`]}
         icon={<Icon.Info cls_hlp="ml-3 w-[30px] h-[30px]" />}
       />
-      <div
-        dangerouslySetInnerHTML={{ __html: data?.[`description_${lang}`] }}
-      />
+      <div dangerouslySetInnerHTML={bioContent} />
     </>
   );
 }
